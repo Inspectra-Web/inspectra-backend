04fff0fb3969a6b89c3fb6416cc7d16a9736c5d7
Inspectra-Web <support@inspectraweb.com>
2025-07-12 22:40:54 +0100

    Made changes to the canCreateListings function

--- helpers/planHelpers.js
+++ helpers/planHelpers.js
@@ -10,25 +10,28 @@ export const canCreateListing = async (userId, type = 'normal') => {
 
   const subscription = await Subscription.findOne({
     user: userId,
-    subscriptionStatus: 'active',
     paymentStatus: 'successful',
-    subscriptionEndDate: { $gte: new Date() },
   }).populate('plan');
 
-  if (subscription?.hasLifeTimeAccess) return { allowed: true, subscription };
+  if (!subscription) return { allowed: false, message: 'No subscription found' };
 
-  if (!subscription) return { allowed: false, message: 'No active subscription found' };
+  if (
+    subscription?.hasLifeTimeAccess ||
+    (subscription.subscriptionStatus === 'active' && subscription.subscriptionEndDate >= new Date())
+  ) {
+    const { listingsUsed, featuredListingUsed } = subscription.usage;
+    const { maxListings, featuredListings } = subscription.plan.features;
 
-  const { listingsUsed, featuredListingUsed } = subscription.usage;
-  const { maxListings, featuredListings } = subscription.plan.features;
+    if (type === 'featured') {
+      if (featuredListingUsed >= featuredListings)
+        return { allowed: false, message: 'Featured Listing limit reached. Upgrade your plan.' };
+    }
 
-  if (type === 'featured') {
-    if (featuredListingUsed >= featuredListings)
-      return { allowed: false, message: 'Featured Listing limit reached. Upgrade your plan.' };
+    if (listingsUsed >= maxListings)
+      return { allowed: false, message: 'Listing limit reached. Upgrade your plan.' };
+
+    return { allowed: true, subscription };
   }
 
-  if (listingsUsed >= maxListings)
-    return { allowed: false, message: 'Listing limit reached. Upgrade your plan.' };
-
-  return { allowed: true, subscription };
+  return { allowed: false, message: 'No active subscription found' };
 };
